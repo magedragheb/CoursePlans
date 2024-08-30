5a5dcb2b6773c94c228a87dcf9962ff2c3d148a8
Maged <19859654+magedragheb@users.noreply.github.com>
2024-08-30 18:19:08 +0300

    refactor

--- CoursePlans.API/Companies/CompanyEndpoints.cs
+++ CoursePlans.API/Companies/CompanyEndpoints.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using CoursePlans.API.Companies.Entities;
 using CoursePlans.API.Data;
 using CoursePlans.API.Endpoints;
+using CoursePlans.API.Endpoints.Filters;
 
 namespace CoursePlans.API.Companies.Endpoints;
 
@@ -13,20 +14,29 @@ public class CompanyEndpoints : IEndpoint
             .WithTags("Companies")
             .WithOpenApi();
 
-        group.MapGet("/", async (CoursePlansContext db) =>
-            await db.Companies.AsNoTracking().ToListAsync())
+        group.MapGet("/", GetCompanies)
             .WithName("GetAllCompanies")
             .WithSummary("Get all Companies");
 
-        group.MapPost("/", async (CompanyDTO companyDTO, CoursePlansContext db) =>
+        group.MapPost("/", AddCompany)
+            .WithName("AddCompany")
+            .WithSummary("Add a new Company")
+            .Produces<Company>(StatusCodes.Status201Created)
+            .Produces(StatusCodes.Status400BadRequest)
+            .WithValidation<Company>();
+    }
+
+    public async Task<IResult> GetCompanies(CoursePlansContext db) =>
+        Results.Ok(await db.Companies.AsNoTracking().ToListAsync());
+
+    public async Task<IResult> AddCompany(CompanyDTO companyDTO, CoursePlansContext db)
+    {
+        var company = new Company
         {
-            var company = new Company
-            {
-                Name = companyDTO.Name
-            };
-            db.Companies.Add(company);
-            await db.SaveChangesAsync();
-            return Results.Created($"/api/companies/{company.Id}", company);
-        });
+            Name = companyDTO.Name
+        };
+        db.Companies.Add(company);
+        await db.SaveChangesAsync();
+        return TypedResults.Created($"/api/companies/{company.Id}", company);
     }
 }
